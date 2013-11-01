3635772689ea9e68c0d1a0610dec297b4d6085d0
Markus Bader <markus.bader@mni.thm.de>
2013-11-01 11:52:01 +0100

    truth table is now handled as relation (except the printing)

--- src/logic/truthtable.clj
+++ src/logic/truthtable.clj
@@ -1,11 +1,31 @@
 (ns logic.util)
 
+(defn ast-to-truth-table
+  "Takes an instaparse-generated ast and generates the truth table for it.
+   Output format (truth table is a relation):
+   [:symbols [A B] \"vector of all formula variables, ex. \"A & B\"\"
+    :table [[true true true] [true false false]...] \"list of lists representing a row\"
+   ]"
+  [ast]
+  (let [symbols (find-symbols ast)
+        clj-code (transform-ast ast)
+        allcomb (selections [true false] (count symbols))
+        assign-map (for [comb allcomb]
+                            (vec (interleave symbols comb)))]
+    [:symbols (vec symbols)
+     :table (vec (for [curr-valuation assign-map]
+					    (let [curr-val-map (apply hash-map curr-valuation)]
+						      (conj (vec (for [sym symbols] 
+                               (sym curr-val-map)))
+                    (eval-formula clj-code curr-valuation))
+						   )))]))
+
 (defn abbrev-bool 
   "Replaces 'true' with 'T' and 'false' with 'F' for better reading of a truth table."
   [value]
   (cstr/replace (cstr/replace value "true" "T") "false" "F"))
 
-(defn print-tt 
+(defn- print-tt 
   "Prints a truth table in a nice format."
   [symbols assign-map formula original-formula]
 
