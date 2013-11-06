d946d38b148f7f34ef38ee54d9c7c0e9974c81c2
Markus Bader <markus.bader@mni.thm.de>
2013-11-06 11:51:45 +0100

    tseitin transformation: variable substitution implemented

--- src/logic/tseitin.clj
+++ src/logic/tseitin.clj
@@ -1,9 +1,5 @@
 (ns logic.util)
 
-(defn tseitin-transform
-  "Takes a formula in clojure code and applies the tseitin transfomation to it"
-  [formula]
-  (all-subformulas formula))
 
 (defn all-subformulas
 	"Takes a formula in clojure code and finds all subformulas.
@@ -23,3 +19,50 @@
         (all-subformulas (nth formula 2)))
     )
 ))
+
+(defn- subs-variables
+  "Takes a formula in clojure code and a map of the form p_n -> phi and substitutes
+   all yet defined variables in that formula recursively. Used by make-variables."
+  [formula rev-map]
+  ; if not on last level
+  (if (and (not= (class formula) java.lang.Boolean) (not (symbol? formula)))
+    ; then
+    (if (contains? rev-map formula)
+      ; then: substitute
+      (rev-map formula)
+      
+      ; else: go on recursively
+      (remove nil? (concat
+                     (list (first formula))
+	                   (list (subs-variables (second formula) rev-map))
+	                   (list (when (not= (name (first formula)) "not")
+	                           (subs-variables (nth formula 2) rev-map)))))
+    )
+    ; else
+    formula)
+)
+
+(defn make-variables
+  "Take a list of all subformulas and creates a list of new variables for them"
+  [subforms]
+  (let [forms (sort-by (comp count flatten) subforms)
+        var-map (zipmap (map #(keyword (str "p_" %)) (range 0 (count forms))) forms)
+        var-keys (sort (keys var-map))]
+       
+    ; substitute each subformula if it already has a variable assigned
+    (apply merge
+	    (for [k var-keys
+            :let [map-wo-formula (dissoc var-map k)]]
+	      (hash-map k (subs-variables 
+                     (var-map k) 
+                     (zipmap (vals map-wo-formula) (keys map-wo-formula))
+                     )))))
+  )
+
+(defn tseitin-transform
+  "Takes a formula in clojure code and applies the tseitin transfomation to it"
+  [formula]
+  (let [subforms (all-subformulas formula)
+        var-map (make-variables subforms)]
+    var-map
+    ))
