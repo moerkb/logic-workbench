3fe0ac0708c7254f083132619172bddf7ef89954
Markus Bader <markus.bader@mni.thm.de>
2013-11-06 15:40:14 +0100

    not reduction implemented

--- src/logic/cnf.clj
+++ src/logic/cnf.clj
@@ -102,7 +102,52 @@
 
     formula))
      
+(defn reduce-not
+  "Takes a formula in clojure code and reduces all negations in the form, e. g.
+     (not (not expr)) -> expr
+     (not false) -> true"
+  [formula]
+  (if (and (not (boolean? formula)) (not (symbol? formula)))
+    (let [op (first formula)
+          a (second formula)]
+
+      (if (= (name op) "not")
+        ; reduce not
+        (cond
+          ;case boolean
+          (and (not (symbol? a)) (boolean? a))
+          
+          (not a)
+          
+          ;case another not
+          (and 
+            (not (symbol? a)) 
+            (not (boolean? a))
+            (= (-> a first name) "not"))
+          
+          (do 
+            (println "Hello" )
+          (reduce-not (second a)))
+            
+          :else (remove nil? (list 
+	                           op
+	                           (reduce-not a)
+	                           (when (not= (name op) "not")
+	                             (reduce-not (nth formula 2)))
+	                           )))
+          
+        ; else
+        (remove nil? (list 
+                       op
+                       (reduce-not a)
+                       (when (not= (name op) "not")
+                         (reduce-not (nth formula 2)))
+                       ))
+      ))
+    ; else
+    formula))
+  
 (defn transform-cnf 
   "Takes a formula in clojure code and produces the cnf of it"
   [formula]
-  (-> formula only-and-or-not not-atoms-only))
+  (-> formula only-and-or-not not-atoms-only reduce-not))
