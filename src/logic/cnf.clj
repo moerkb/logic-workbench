12eda635f2c5427d9a2fa8e47067822634bc9208
Markus Bader <markus.bader@mni.thm.de>
2013-11-13 18:39:22 +0100

    new impl-free function

--- src/logic/cnf.clj
+++ src/logic/cnf.clj
@@ -1,5 +1,33 @@
 (ns logic.util)
 
+; new try
+; TODO impl expands to macro (if (not A) true B) - should that be?
+
+(defn impl-free [formula]
+    (if (literal? formula)
+      formula
+
+		  (let [op (first formula)
+		        name-op (name op)]
+	      (if (or
+	            (= name-op "and")
+	            (= name-op "or")
+	            (= name-op "not"))
+	        (concat
+	          (list op)
+	            (for [elem (rest formula)]
+	              (impl-free elem)
+	            ))
+	          (let [expform (macroexpand-1 formula)]
+	            (concat
+	              (list (first expform))
+	              (for [elem (rest expform)]
+	                (impl-free elem)
+	              )))))))
+
+
+; old try
+
 (declare only-and-or-not)
 
 (def cnf-trans-map
