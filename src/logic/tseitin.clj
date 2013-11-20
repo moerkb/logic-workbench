9ef7f9f38d0df2000a78af6c32a220016f6c6eea
Markus Bader <markus.bader@mni.thm.de>
2013-11-20 14:11:59 +0100

    tseitin transformation produces cnf, but not for all ops or arbitrary number of arguments of logical function

--- src/logic/tseitin.clj
+++ src/logic/tseitin.clj
@@ -1,18 +1,57 @@
 (ns logic.util)
 
+; TODO make this function for multiple arguments
+; TODO make this function for all operators
+
+(defn- tseitin-cnf
+  "Takes a formula with tseitin symbols, the tseitin symbol for this formula
+   and substitutes the formula with cnf.
+   Currently binary only!"
+  [formula tsym]
+  (if (literal? formula)
+    formula
+    (let [[op a] formula]
+      (if (= op 'not)
+        (list 'and
+          (list 'or
+            (list 'not tsym)
+            (list 'not a)))
+      
+        (let [b (nth formula 2)]
+          (case op
+            and (list 'and
+                  (list 'or (list 'not tsym) a)
+                  (list 'or (list 'not tsym) b)
+                  (list 'or tsym (list 'not a) (list 'not b)))
+          
+            or (list 'and
+                 (list 'or tsym (list 'not a))
+                 (list 'or tsym (list 'not b))
+                 (list 'or (list 'not tsym) (list 'not a) b))
+          
+            impl (list 'and
+                   (list 'or tsym a)
+                   (list 'or tsym (list 'not b))
+                   (list 'or (list 'not tsym) (list 'not a) b))
+          
+            ;default
+            (println "Tseitin-CNF not implemented for" op)))))))
+
 (defn- generate-tseitin-symbols
   "Recursive function for tseitin conversion to generate the new symbols."
   [formula tmap]
     (if (literal? formula)
       [formula tmap]
       (let [rem-retvals (map #(generate-tseitin-symbols % tmap) (rest formula))
-            new-formula (apply list (first formula) (map first rem-retvals))
+            new-sym (gensym tseitin-prefix)
+            new-formula (tseitin-cnf 
+                          (apply list (first formula) (map first rem-retvals))
+                          new-sym)
             new-tmap (reduce merge (map second rem-retvals))
-            new-sym (gensym tseitin-prefix)]
+            ]
 
         [new-sym (conj new-tmap [new-sym new-formula])]
       )))
-                      
 
 (defn tseitin-transform
   "Takes a formula in clojure code and applies the tseitin transformation to it."
@@ -20,8 +59,6 @@
   (let [gen-list (generate-tseitin-symbols formula {})]
     (apply list 'and
       (first gen-list)
-      (map
-        #(list 'equiv (first %) (second %))
-        (second gen-list)))
+      (vals (second gen-list)))
   )
 )
