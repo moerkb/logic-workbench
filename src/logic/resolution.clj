9875403b913cc6934313c6f220bd86d7e734439c
br <br@192.168.0.109>
2013-12-11 10:17:30 +0100

    resolution notes.txt syntax of fol

--- src/logic/resolution.clj
+++ src/logic/resolution.clj
@@ -60,4 +60,22 @@
 (resolvents #{1} #{-1 3} #{1 2} #{-3})
 (resolvents #{1 2} #{-1 3} #{-1 -3} #{-1 -2})
 
+
+(defn resolution
+  "Return :unsat if the given set of clauses is unsatisfiable,
+   :sat otherwise"
+  [cl-set]
+  (let [res (apply resolvents cl-set) ext-cl-set (set/union cl-set res)]
+    (cond
+	    (contains? res #{}) :unsat
+      (= cl-set ext-cl-set) :sat
+      :else (recur ext-cl-set))))
+     
+; examples
+(resolution #{#{1} #{-1}})
+(resolution #{#{1} #{2}})
+(resolution #{(clause [1 2]) (clause [-1 2]) (clause [1 -2]) (clause [-1 -2])})
+(resolution #{(clause [1]) (clause [-1 2]) (clause [-2])})
+(resolution #{(clause [-1 2]) (clause [-3 4]) (clause [1]) (clause [3]) (clause [-4 -2])})
+    
           
