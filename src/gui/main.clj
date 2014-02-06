693e8769117ccd626570c855dcf8b07972188fa6
Markus Bader <markus.bader@mni.thm.de>
2014-02-06 19:43:18 +0100

    gui: menu structure added

--- src/gui/main.clj
+++ src/gui/main.clj
@@ -1,10 +1,20 @@
 (ns gui.main
   (require [seesaw.core :refer :all]
            [seesaw.rsyntax :as syntax]
-           [seesaw.dev :refer (show-options)]))
+           [seesaw.dev :refer (show-options)]
+           [seesaw.util :refer (to-mnemonic-keycode)])
+  (import [java.io File]))
 
 ; general stuff
 (native!)
+
+(def icon-folder "resources/icons/")
+
+(defn icon-path
+  "Takes a file name and gives the file object with the relative path to the correct icon folder, e.g.
+  (icon-file \"save.gif\") does the same as (File. resources\\icons\\save.gif)."
+  [file-name]
+  (File. (str icon-folder file-name)))
 
 ; main window building
 (def form-tree (scrollable (tree)
@@ -21,7 +31,8 @@
 (def tool-bar (toolbar
                 :floatable? false
                 :orientation :horizontal
-                :items [(button :text "Truth Table")]))
+                :items [(button :text "Truth Table")
+                        (button :text "SAT-Solve")]))
 
 (def ver-panel (vertical-panel
                  :items [form-editor
@@ -31,22 +42,72 @@
                  :items [form-tree
                          ver-panel]))
 
-(def main-panel (vertical-panel
-                  :items [tool-bar
-                          hor-panel]))
+(def main-panel (border-panel
+                  :north tool-bar
+                  :center hor-panel))
 
+; menus
+(def project-menu (menu 
+                    :text "Project"
+                    :items [(action :name "New Project")
+                            (action :name "Open Project")
+                            (separator)
+                            (action :name "New Proposition")
+                            (action :name "Rename Proposition")
+                            (separator)
+                            (action :name "New M4 File")
+                            (action :name "Open M4 File")
+                            (separator)
+                            (action 
+                              :name "Save" 
+                              :key "ctrl S" 
+                              :mnemonic "s"
+                              :icon (icon-path "save.gif")
+                              :handler (fn [_] (println "Saving")))
+                            (action 
+                              :name "Save All"
+                              :key "ctrl shift S"
+                              :mnemonic "a"
+                              :icon (icon-path "saveall.gif")
+                              :handler (fn [_] (println "Saving All"))
+                              )
+                            (action :name "Close")
+                            (action :name "Delete")
+                            (separator)
+                            (action :name "Exit")]))
+
+(def tasks-menu (menu
+                  :text "Tasks"
+                  :items [(action :name "Check SAT")
+                          (action :name "Truth Table")
+                          (action :name "Transform to CNF")
+                          (action :name "Tseitin Transformation")
+                          (action :name "Generate DIMACS")
+                          (action :name "Preprocess with M4")
+                  ]))
+
+(def options-menu (menu
+                    :text "Options"
+                    :items [(action :name "Select Font")
+                            (action :name "Settings")
+                            (action :name "Include Path")]))
+
+(def help-menu (menu
+                 :text "Help"
+                 :items [(action :name "Help Contents")
+                         (separator)
+                         (action :name "About")]))
+
+; we're rolling
 (def main-frame (frame 
                   :title "Logical Workbench"
                   :on-close :exit
                   :size [980 :by 600]
                   :content main-panel
                   :menubar (menubar 
-                             :items [(menu :text "Project")
-                                     (menu :text "Tasks")
-                                     (menu :text "Settings")
-                                     (menu :text "Help")])))
+                             :items [project-menu
+                                     tasks-menu
+                                     options-menu
+                                     help-menu])))
 
-(.setAlignmentX tool-bar (java.awt.Component/LEFT_ALIGNMENT))
-
-; we're rolling
 (-> main-frame pack! show!)
