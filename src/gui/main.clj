2b206c23dd9baa3445fc122b86747e30e4f66e80
Markus Bader <markus.bader@mni.thm.de>
2014-02-07 16:40:54 +0100

    Gui: menu icons added

--- src/gui/main.clj
+++ src/gui/main.clj
@@ -30,8 +30,35 @@
 (def tool-bar (toolbar
                 :floatable? false
                 :orientation :horizontal
-                :items [(button :text "Truth Table")
-                        (button :text "SAT-Solve")]))
+                :items [(button :icon (icon-path "new_project.gif"))
+                        (button :icon (icon-path "open_project.gif"))
+                        ;(separator :orientation :vertical)
+                        (button :icon (icon-path "new_proposition.gif"))
+                        (button :icon (icon-path "rename_proposition.gif"))
+                        ;(separator :orientation :vertical)
+                        (button :icon (icon-path "new_m4file.gif"))
+                        (button :icon (icon-path "open_m4file.gif"))
+                        ;(separator :orientation :vertical)
+                        (button :icon (icon-path "save.gif"))
+                        (button :icon (icon-path "save_all.gif"))
+                        (button :icon (icon-path "close.gif"))
+                        (button :icon (icon-path "delete.gif"))
+                        
+                        ;(separator :orientation :vertical)
+                        
+                        (button :icon (icon-path "task_sat.gif"))
+                        (button :icon (icon-path "task_tt.gif"))
+                        (button :icon (icon-path "task_cnf.gif"))
+                        (button :icon (icon-path "task_tcnf.gif"))
+                        (button :text "Dimacs")
+                        (button :icon (icon-path "task_m4.gif"))
+                        
+                        ;(separator :orientation :vertical)
+                        
+                        (button :icon (icon-path "select_font.gif"))
+                        (button :icon (icon-path "settings.gif"))
+                        ]))
+
 
 (def ver-panel (vertical-panel
                  :items [form-editor
@@ -49,49 +76,94 @@
 (def project-menu (menu 
                     :text "Project"
                     :mnemonic "P"
-                    :items [(action :name "New Project")
-                            (action :name "Open Project")
+                    :items [(action 
+                              :name "New Project"
+                              :key "ctrl shift N"
+                              :icon (icon-path "new_project.gif"))
+                            (action 
+                              :name "Open Project"
+                              :key "ctrl shift O"
+                              :icon (icon-path "open_project.gif"))
                             (separator)
-                            (action :name "New Proposition")
-                            (action :name "Rename Proposition")
+                            (action 
+                              :name "New Proposition"
+                              :key "ctrl N"
+                              :icon (icon-path "new_proposition.gif"))
+                            (action 
+                              :name "Rename Proposition"
+                              :key "ctrl R"
+                              :icon (icon-path "rename_proposition.gif"))
                             (separator)
-                            (action :name "New M4 File")
-                            (action :name "Open M4 File")
+                            (action 
+                              :name "New M4 File"
+                              :key "alt N"
+                              :icon (icon-path "new_m4file.gif"))
+                            (action 
+                              :name "Open M4 File"
+                              :key "alt O"
+                              :icon (icon-path "open_m4file.gif"))
                             (separator)
                             (action 
                               :name "Save" 
                               :key "ctrl S" 
                               :mnemonic "S"
                               :icon (icon-path "save.gif")
-                              :handler (fn [_] (println "Saving")))
+                              :handler (fn [_] (println "Saving"))) ;TODO
                             (action 
                               :name "Save All"
                               :key "ctrl shift S"
                               :mnemonic "A"
-                              :icon (icon-path "saveall.gif")
-                              :handler (fn [_] (println "Saving All"))
+                              :icon (icon-path "save_all.gif")
+                              :handler (fn [_] (println "Saving All")) ;TODO
                               )
-                            (action :name "Close")
-                            (action :name "Delete")
+                            (action 
+                              :name "Close"
+                              :icon (icon-path "close.gif"))
+                            (action 
+                              :name "Delete"
+                              :icon (icon-path "delete.gif"))
                             (separator)
-                            (action :name "Exit")]))
+                            (action 
+                              :name "Exit"
+                              :key "ctrl Q"
+                              :icon (icon-path "exit.gif"))]))
 
 (def tasks-menu (menu
                   :text "Tasks"
                   :mnemonic "T"
-                  :items [(action :name "Check SAT")
-                          (action :name "Truth Table")
-                          (action :name "Transform to CNF")
-                          (action :name "Tseitin Transformation")
-                          (action :name "Generate DIMACS")
-                          (action :name "Preprocess with M4")
+                  :items [(action 
+                            :name "Check SAT"
+                            ;:key "ctrl T+S"
+                            :icon (icon-path "task_sat.gif"))
+                          (action 
+                            :name "Truth Table"
+                            ;:key "ctrl T T"
+                            :icon (icon-path "task_tt.gif"))
+                          (action 
+                            :name "Transform to CNF"
+                            ;:key "ctrl T C"
+                            :icon (icon-path "task_cnf.gif"))
+                          (action 
+                            :name "Tseitin Transformation"
+                            :icon (icon-path "task_tcnf.gif"))
+                          (action 
+                            :name "Generate DIMACS")
+                          (action 
+                            :name "Preprocess with M4"
+                            ;:key "ctrl T M"
+                            :icon (icon-path "task_m4.gif"))
                   ]))
 
 (def options-menu (menu
                     :text "Options"
                     :mnemonic "O"
-                    :items [(action :name "Select Font")
-                            (action :name "Settings")
+                    :items [(action 
+                              :name "Select Font"
+                              :icon (icon-path "select_font.gif"))
+                            (action 
+                              :name "Settings"
+                              :key "alt S"
+                              :icon (icon-path "settings.gif"))
                             (action :name "Include Path")]))
 
 (def help-menu (menu
