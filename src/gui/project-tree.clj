ed431db1a0b065ccfbe838f9f1bdd414338867de
Daniel Kirsten <daniel.kirsten@mni.thm.de>
2014-03-27 15:57:46 +0100

    Added preparation of m4 files in node

--- src/gui/project-tree.clj
+++ src/gui/project-tree.clj
@@ -1,21 +1,5 @@
 (ns gui.main)
 
-; A hand made project tree example
-;(def test-tree
-;  (Node. "Projects"
-;         nil
-;         (list
-;           (Node. "Project_1"
-;                  "This is a test project."
-;                  (list
-;                    (Node. "Formel_1" "a & b")
-;                    (Node. "Formel_2" "a -> b")))
-;           (Node. "Project_2"
-;                  ""
-;                  (list
-;                    (Node. "Formel_3" "a <-> b")
-;                    (Node. "Formel_4" "a !| b"))))))
-
 (defn file2node
   [f]
   (std-catch
@@ -53,7 +37,7 @@
     
     (Node. "Projects"
            nil
-           "true"
+           nil
          (if (first file-vector)
              (get-project-list file-vector)
              nil))))
@@ -63,7 +47,7 @@
 
 (def tree-model
   (let [children #(.children %)
-        branch? #(.path %)
+        branch? #(.isProject %)
         tree tree-of-projects]
     (set-setting
           :project-tree
